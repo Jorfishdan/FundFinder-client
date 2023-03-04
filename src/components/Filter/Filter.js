b3f8073966273a3a4bca3a8c4c68478b6e81eef7
Jorfishdan <jordan_fish@ymail.com>
2023-03-04 14:46:27 -0800

    styling of filter bar applied

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,15 +1,24 @@
 import './Filter.scss';
+import arrow from "../../assets/images/arrow2.png";
 function Filter() {
 return (<>
 <section className="filter">
     <div className="filter__wrapper">
-    <article className="filter__type">Type</article>
-    <article className="filter__location">Location</article>
-    <article className="filter__gender">Gender</article>
-    <article className="filter__location">Location</article>
-    <article className="filter__date">Date Posted</article>
-    <article className="filter__search">Search</article>
-    <article className="filter__rest">Reset</article>
+    <article className="filter__item">Type 
+    <img src={arrow} alt="down arrow to open options" className="filter__arrow" />
+    </article>
+    <article className="filter__item">Location
+    <img src={arrow} alt="down arrow to open options" className="filter__arrow" /></article>
+    <article className="filter__item">Gender
+    <img src={arrow} alt="down arrow to open options" className="filter__arrow" /></article>
+    <article className="filter__item">Location
+    <img src={arrow} alt="down arrow to open options" className="filter__arrow" /></article>
+    <article className="filter__item">Date Posted
+    <img src={arrow} alt="down arrow to open options" className="filter__arrow" /></article>
+    <article className="filter__search">Search
+    </article>
+    <article className="filter__reset">Reset
+    </article>
     </div>
 
 </section>
