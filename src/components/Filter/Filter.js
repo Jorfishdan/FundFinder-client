7dfbc8c5019db3949d15074ba9514cafa1094dd8
Jorfishdan <jordan_fish@ymail.com>
2023-03-04 14:21:52 -0800

    set up classes for filter component

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,19 @@
+import './Filter.scss';
+function Filter() {
+return (<>
+<section className="filter">
+    <div className="filter__wrapper">
+    <article className="filter__type">Type</article>
+    <article className="filter__location">Location</article>
+    <article className="filter__gender">Gender</article>
+    <article className="filter__location">Location</article>
+    <article className="filter__date">Date Posted</article>
+    <article className="filter__search">Search</article>
+    <article className="filter__rest">Reset</article>
+    </div>
+
+</section>
+</>)
+}
+
+export default Filter;
