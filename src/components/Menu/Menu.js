e4fff2e01eac157a40cdd03ab4515776fa791092
Hilary <hilarybrittonfoster@gmail.com>
2023-03-04 16:56:13 -0800

    first menu modal done

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,12 +1,31 @@
-import "./Menu.scss";
-
-import React from 'react'
+import Gears from "../../assets/icons/setting.png";
+import Email from "../../assets/icons/contact.png";
+import Logout from "../../assets/icons/logout.png";
 
 export default function Menu() {
   return (
     <main className="menu__container">
-        <h1 className="menu__name">Hi, Username</h1>
-      
+      <h1 className="menu__name">Hi, Username</h1>
+      <div className="menu__box">
+        <p className="menu__label">Setting</p>
+        <img className="menu__img" src={Gears} alt="Setting Icon" />
+      </div>
+      <div className="menu__box">
+        <p className="menu__label">Contact</p>
+        <img
+          className="menu__img  menu__img-contact"
+          src={Email}
+          alt="Contact Icon"
+        />
+      </div>
+      <div className="menu__box">
+        <p className="menu__label">Logout</p>
+        <img
+          className="menu__img  menu__img-logout"
+          src={Logout}
+          alt="Logout Icon"
+        />
+      </div>
     </main>
-  )
+  );
 }
