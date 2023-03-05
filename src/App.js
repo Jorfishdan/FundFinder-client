8e2a39e508fae02d266361b52f195fa93e0967a3
Kimberlynolsin <kimberly.nolsin@gmail.com>
2023-03-04 20:56:35 -0500

    added post methods to signup page

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import './styles/styles.scss'
 
-function App() {
-  const [user, setUser] = useState({});
 
+
+function App() {
+  const URL = process.env.REACT_APP_BASE_URL;
+  const login = '/login'
+  const signup = '/signup'
+
+  const [user, setUser] = useState({});
+  const [isSignedUp, setIsSignedUp] = useState(false)
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+
+  console.log('APP:',URL);
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Landing user={user} setUser={setUser} />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Landing user={user} setUser={setUser} isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} URL={URL}/>} />
+        <Route path="/signup" element={<Signup isSignedUp={isSignedUp} setIsSignedUp={setIsSignedUp} URL={URL} signup={signup}/>} />
         <Route path="/dashboard" element={<Homepage />} />
       </Routes>
     </Router>
