685aa7d626f13b016b0828398de51bce16910acb
Kimberlynolsin <kimberly.nolsin@gmail.com>
2023-03-05 20:39:39 -0500

    added a google external link to signup page

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -6,6 +6,7 @@ import { useEffect, useRef } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import googleIcon from "../../assets/icons/google.png";
 
 export default function Welcome({
   user,
@@ -50,7 +51,6 @@ export default function Welcome({
       });
   };
 
-  
   useEffect(() => {
     const handleCallbackResponse = (response) => {
       console.log("Encoded JWT ID token:" + response.credential);
@@ -114,8 +114,16 @@ export default function Welcome({
           ></input>
           <button className="main__signin--button">Sign Up</button>
           <hr></hr>
-          <section className="main__google">
-            <div id="signInDiv" className="main__google--box"></div>
+          <section className="signup__google">
+            <Link
+              to="https://accounts.google.com/signup/v2/webcreateaccount?continue=https%3A%2F%2Fmyaccount.google.com%3Futm_source%3Daccount-marketing-page%26utm_medium%3Dcreate-account-button&flowName=GlifWebSignIn&flowEntry=SignUp"
+              target="_blank"
+            >
+              <div className="signup__google--signup">
+                <p className="signup__google--text">Sign Up with Google</p>
+                <img className="signup__google--icon" src={googleIcon}></img>
+              </div>
+            </Link>
           </section>
           <Link to="/dashboard" className="main__google--guest">
             CONTINUE AS GUEST
